7fe1bb904cfd4dc439dce41064cc09802b3ca832
workingdiagnosis <42192332+workingdiagnosis@users.noreply.github.com>
2018-08-08 20:25:22 -0400

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -28,6 +28,7 @@
         $.when(pt, obv).fail(onError);
 
         $.when(pt, obv).done(function(patient, obv) {
+          console.log(obv);
           var byCodes = smart.byCodes(obv, 'code');
           var gender = patient.gender;
 
