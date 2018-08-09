40a4b1b70fbabbdf3747435ffb510c6cfdb0828c
workingdiagnosis <42192332+workingdiagnosis@users.noreply.github.com>
2018-08-08 20:24:04 -0400

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -16,13 +16,13 @@
         console.log(pt);
         var obv = smart.patient.api.fetchAll({
                     type: 'Observation',
-                    query: {
-                      code: {
-                        $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
-                              'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
-                      }
-                    }
+//                     query: {
+//                       code: {
+//                         $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
+//                               'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
+//                               'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
+//                       }
+//                     }
                   });
 
         $.when(pt, obv).fail(onError);
